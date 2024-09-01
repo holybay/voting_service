4bbb8142a6a0d6a2ef181e5108ce285abd9a4fff
Vlad Golubev <holybay1886@gmail.com>
2024-09-01 17:08:12 +0300

    Create Artist Storage Interface and Impl and Artist entity

---
+++ src/main/java/by/it_academy/jd2/golubev_107/voting_service/storage/IArtistStorage.java
@@ -0,0 +1,16 @@
+package by.it_academy.jd2.golubev_107.voting_service.storage;
+
+import by.it_academy.jd2.golubev_107.voting_service.storage.entity.Artist;
+
+import java.util.List;
+
+public interface IArtistStorage {
+
+    void create(Artist artist);
+
+    Artist readById(Long id);
+
+    List<Artist> readByName(String name);
+
+    List<Artist> readAll();
+}
