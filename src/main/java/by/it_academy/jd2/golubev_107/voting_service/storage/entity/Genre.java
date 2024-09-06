ee9153b0aab19725647212b5669c7f9f0fb60819
Vlad Golubev <holybay1886@gmail.com>
2024-09-06 17:32:42 +0300

    Added a Genre Entity

---
+++ src/main/java/by/it_academy/jd2/golubev_107/voting_service/storage/entity/Genre.java
@@ -0,0 +1,54 @@
+package by.it_academy.jd2.golubev_107.voting_service.storage.entity;
+
+import java.util.Objects;
+
+public class Genre {
+
+    private Long id;
+    private String name;
+
+    public Genre() {
+    }
+
+    public Genre(Genre genre) {
+        this.id = genre.id;
+        this.name = genre.name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Genre genre = (Genre) o;
+        return Objects.equals(id, genre.id) && Objects.equals(name, genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
