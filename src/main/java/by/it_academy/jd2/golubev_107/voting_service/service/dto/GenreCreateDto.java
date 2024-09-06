f39f3d40883f8b79d8443de205a9c2a3dc2e896a
Vlad Golubev <holybay1886@gmail.com>
2024-09-06 17:34:12 +0300

    Created 2 GenreService dtos

---
+++ src/main/java/by/it_academy/jd2/golubev_107/voting_service/service/dto/GenreCreateDto.java
@@ -0,0 +1,36 @@
+package by.it_academy.jd2.golubev_107.voting_service.service.dto;
+
+import java.util.Objects;
+
+public class GenreCreateDto {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GenreCreateDto that = (GenreCreateDto) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "GenreCreateDto{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
