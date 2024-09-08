0b8bfe7d3f5ff59b176b5d91b9a4c8e9526f547e
Vlad Golubev <holybay1886@gmail.com>
2024-09-09 00:29:58 +0300

    Removed unused classes and methods after the rework

--- src/main/java/by/it_academy/jd2/golubev_107/voting_service/util/Util.java
+++ src/main/java/by/it_academy/jd2/golubev_107/voting_service/util/Util.java
@@ -1,22 +1,10 @@
 package by.it_academy.jd2.golubev_107.voting_service.util;
 
-import by.it_academy.jd2.golubev_107.voting_service.storage.entity.EGenre;
-
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class Util {
 
     public static final int MIN_PARAMS_COUNT = 1;
-
-    public static List<EGenre> toEGenresList(String[] genres) {
-        List<EGenre> eGenres = new ArrayList<>(genres.length);
-        for (String genre : genres) {
-            eGenres.add(EGenre.valueOf(genre));
-        }
-        return eGenres;
-    }
 
     public static String setDynamicSqlParams(String sql, final int paramsCount) {
         if (paramsCount < MIN_PARAMS_COUNT) {
